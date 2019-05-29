9fa5d17ca0134f927264d37f878d1737c584efc9
khanhpn <khanhpham@bravesoft.vn>
2019-05-29 07:12:48 +0700

    include npm lib

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
+//= require bootstrap/dist/js/bootstrap.min
 //= require activestorage
 //= require turbolinks
 //= require_tree .
