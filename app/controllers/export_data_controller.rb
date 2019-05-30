410317c0fe9c38ef6ba702ee6b39c5912e53dff3
khanhpn <khanhpham@bravesoft.vn>
2019-05-30 08:32:49 +0700

    implement export excels

--- app/controllers/export_data_controller.rb
+++ app/controllers/export_data_controller.rb
@@ -2,6 +2,8 @@ class ExportDataController < ApplicationController
   def index; end
 
   def execute
-
+    axlsx = CameoAxlsx.new(User.all, params["name_file"], params["name_sheet"])
+    axlsx.execute
+    redirect_to export_data_path
   end
 end
