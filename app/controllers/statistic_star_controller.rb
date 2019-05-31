52a9af424357f5160d8abe2d6677e17e20b38a1e
khanhpn <khanhpham@bravesoft.vn>
2019-05-31 08:02:01 +0700

    implement statistic stars

--- app/controllers/statistic_star_controller.rb
+++ app/controllers/statistic_star_controller.rb
@@ -1,11 +1,14 @@
 class StatisticStarController < ApplicationController
   def index
-    @current_week = "#{Date.today.beginning_of_week.strftime('%Y/%m/%d')} - #{Date.today.end_of_week.strftime('%Y/%m/%d')}"
-    # @pagy, @categories = pagy(Category.all.includes(:users).search(@current_week))
-    @pagy, @categories = pagy(Category.all.includes(:users))
+    @start_time = Date.today.beginning_of_week
+    @end_time = Date.today.end_of_week
+    @pagy, @categories = pagy(Category.all.includes(:users).search(@start_time, @end_time))
+    # @pagy, @categories = pagy(Category.all.includes(:users))
   end
 
   def search
-
+    @start_time = Date.today.beginning_of_week
+    @end_time = Date.today.end_of_week
+    @pagy, @categories = pagy(Category.all.includes(:users).search(@start_time, @end_time))
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def datetime_range(start_week, end_week)
+    "#{start_week.strftime('%Y/%m/%d')} - #{end_week.strftime('%Y/%m/%d')}"
+  end
 end
