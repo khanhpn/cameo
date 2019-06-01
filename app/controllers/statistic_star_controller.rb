946c6fd356b915f4b25ece9ee7cbc6c8424f0e3f
khanhpn <khanhpham@bravesoft.vn>
2019-06-01 10:18:53 +0700

    implement statistic stars

--- app/controllers/statistic_star_controller.rb
+++ app/controllers/statistic_star_controller.rb
@@ -1,14 +1,19 @@
 class StatisticStarController < ApplicationController
+  before_action :set_params
+
   def index
-    @start_time = Date.today.beginning_of_week
-    @end_time = Date.today.end_of_week
     @pagy, @categories = pagy(Category.all.includes(:users).search(@start_time, @end_time))
   end
 
-  def search
-    datetime = params["date_range"].split("-").map(&:squish).map(&:to_datetime)
-    @start_time = datetime.dig(0)
-    @end_time = datetime.dig(1)
-    @pagy, @categories = pagy(Category.all.includes(:users).search(@start_time, @end_time))
+  private
+  def set_params
+    if params["date_range"].present?
+      datetime = params["date_range"].split("-").map(&:squish).map(&:to_datetime)
+      @start_time = datetime.dig(0)
+      @end_time = datetime.dig(1)
+    else
+      @start_time = Date.today.beginning_of_week
+      @end_time = Date.today.end_of_week
+    end
   end
 end
