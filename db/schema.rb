a64fd901bce6a8de3c4f0453abe6e155dabd02d6
Augusto Samamé <augustosamame@gmail.com>
2019-06-05 12:32:32 -0500

    fixed temporarilyUnavailable ui

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_29_104451) do
+ActiveRecord::Schema.define(version: 2019_06_05_134915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.string "full_name"
+    t.integer "priority"
     t.integer "number_user"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -70,6 +72,7 @@ ActiveRecord::Schema.define(version: 2019_05_29_104451) do
     t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "temporarilyUnavailable"
     t.index ["_id"], name: "index_users_on__id"
   end
 
